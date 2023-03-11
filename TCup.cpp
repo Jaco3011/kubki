d2620e3e079e8f40087e6bcf0b05a29c77f94b92
Jacek <holzejow@student.agh.edu.pl>
2023-03-11 23:15:54 +0100

    On branch master  Changes to be committed: 	modified:   TCup.cpp 	Makefile.win 	lab2.cbp 	lab2.dev 	lab2.layout

--- TCup.cpp
+++ TCup.cpp
@@ -11,16 +11,34 @@ void TCup::show(){
 	int count = substances.size();
 	for (int i = 0; i < count; i++){
 		double mass = substances[i].get_ro() * volumes[i] * 1000; //result in grams
-		cout << substances[i].get_name() 
+		cout << substances[i].get_name()
 		<< "; volume: " << volumes[i] * 1e6
 		<< " ml; mass: " << mass << " g\n";
 	};
 };
 
 void TCup::print_coe_vol(){
-
+    double total_volume = 0;
+    int count = substances.size();
+    for(int i = 0; i<count; i++){
+        total_volume+=volumes[i];
+    }
+    for(int i = 0; i<count; i++){
+        if (i > 0) {cout " : ";}
+        cout << volumes[i]/total_volume;
+    }
+    cout << endl;
 };
 
 void TCup::print_coe_mass(){
-
+    double total_mass = 0;
+    int count = substances.size();
+    for(int i = 0; i<count; i++){
+        total_mass+=volumes[i]*substances[i].get_ro();
+    }
+    for(int i = 0; i<count; i++){
+        if (i > 0) {cout " : ";}
+        cout << volumes[i]*substances[i].get_ro()/total_mass;
+    }
+    cout << endl;
 };
